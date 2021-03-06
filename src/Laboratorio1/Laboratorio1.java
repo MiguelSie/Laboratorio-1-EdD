5df8a28b91ec2bae832b1013bcdcf45f4b6ca6bb
Miguel Sierra <miguel109737@gmail.com>
2021-03-05 20:30:59 -0500

    Iniciando interfaz gráfica

---
+++ src/Laboratorio1/Laboratorio1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Laboratorio1;
+
+/**
+ *
+ * @author migue
+ */
+public class Laboratorio1 {
+
+    public static void main(String[] args) {
+        Modulo M = new Modulo();
+        M.setVisible(true);
+    }
+}
